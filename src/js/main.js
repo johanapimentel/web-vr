c5a5596fe624a82cdc260b7310966c8f988c8d31
Vanessa Pimentel <johanapimentel@gmail.com>
2021-08-10 06:27:14 -0500

    cambiando estilso a pagina

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,7 @@ const viewer = new PANOLENS.Viewer({
 viewer.add(panorama);
 
 const panoImage2 = document.querySelector(".cocina");
-const oliver = "https://i.ibb.co/8mVTd75/oliver2.png";
+const oliver = "https://i.ibb.co/CBpyfvM/oliverversion5.png";
 
 const panorama2 = new PANOLENS.ImagePanorama(oliver);
 const viewer2 = new PANOLENS.Viewer({
@@ -20,12 +20,12 @@ const viewer2 = new PANOLENS.Viewer({
 
 viewer2.add(panorama2);
 
-const panoImage3 = document.querySelector(".cocina");
-const oliver2 = "https://i.ibb.co/CBpyfvM/oliverversion5.png";
+// const panoImage3 = document.querySelector(".cocina");
+// const oliver2 = "https://i.ibb.co/CBpyfvM/oliverversion5.png";
 
-const panorama3 = new PANOLENS.ImagePanorama(oliver2);
-const viewer3 = new PANOLENS.Viewer({
-    container: panoImage3
-});
+// const panorama3 = new PANOLENS.ImagePanorama(oliver2);
+// const viewer3 = new PANOLENS.Viewer({
+//     container: panoImage3
+// });
 
-viewer3.add(panorama3);
+// viewer3.add(panorama3);
